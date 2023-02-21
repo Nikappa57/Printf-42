03c0caa17de7ca3f5d394437e436ec5f566cf7c2
Nikappa57 <lorenz.gaudino@gmail.com>
2023-02-21 12:15:18 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lorenzogaudino <lorenzogaudino@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 18:47:44 by lorenzogaud       #+#    #+#             */
-/*   Updated: 2023/02/16 19:53:07 by lorenzogaud      ###   ########.fr       */
+/*   Updated: 2023/02/19 22:54:00 by lorenzogaud      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,20 @@
 # include "libft/libft.h"
 
 int		ft_printf(const char *str, ...);
-int		ft_putarg(char type, void *arg);
+int		ft_putarg(char type, va_list *ap);
+
+void	ft_puthex_print(unsigned int n, const char type);
+void	ft_putptr_print(unsigned long n);
+void	ft_putunbr_print(unsigned int n);
+
+int		ft_lenhex(unsigned int n);
+int		ft_lenptr(unsigned long n);
+int		ft_lenunbr(unsigned int nbr);
+
+int		ft_putchar(char c);
+int		ft_putstr(char *str);
+int		ft_puthex(unsigned int n, const char type);
+int		ft_putptr(unsigned long n);
+int		ft_putnbr(int n);
+int		ft_putunbr(unsigned int n);
 #endif
